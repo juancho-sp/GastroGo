9e65ef033f0a7c5850d9a03f7a7f527eb0f98d0e
JuanRojas <ing.juancho.sp@gmail.com>
2025-04-19 08:39:57 -0500

    Se agrega entrega de Spring Java para montar evidencia

---
+++ Spring Java/GastroGo/src/main/java/com/powerdev/GastroGo/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.powerdev.GastroGo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.powerdev.GastroGo.model.Usuario;
+import com.powerdev.GastroGo.service.UsuarioService;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping
+	public String listarUsuarios(Model model) {		
+		model.addAttribute("usuarios", usuarioService.listarTodas());
+		return "home-admin-editar";
+	}
+	
+    @GetMapping("/nuevo")
+    public String mostrarFormularioNuevoUsuario(Model model) {
+        model.addAttribute("usuario", new Usuario()); 
+        return "home-admin-crear";
+    }
+	
+	@PostMapping
+	public String guardarUsuario(Usuario usuario) {
+		usuarioService.guardar(usuario);
+		return "redirect:/usuario";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioEditarUsuario(@PathVariable Long id, Model model) {
+		model.addAttribute("usuario", usuarioService.obtenerPorId(id));
+		return "home-admin-crear";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUsuario(@PathVariable Long id) {
+	    usuarioService.eliminar(id);
+	    return "redirect:/usuario";
+	}
+}
